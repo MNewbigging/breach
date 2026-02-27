22b9b4578cf47ee3adea64d6bf7833a4ecd1e4d9
Matt Newbigging <mrnewbigging@gmail.com>
2026-02-27 20:36:53 +0000

    password feedback

--- src/components/core-access-screen/password-feedback/password-feedback.tsx
+++ src/components/core-access-screen/password-feedback/password-feedback.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useRef } from "react";
 import { CandidateFeedback } from "../core-access-screen";
 import { FeedbackRow } from "./password-feedback-row/password-feedback-row";
 import styles from "./password-feedback.module.scss";
@@ -7,9 +8,21 @@ interface PasswordFeedbackProps {
 }
 
 export function PasswordFeedback({ feedback }: PasswordFeedbackProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const newestFirst = useMemo(() => {
+    return [...feedback].reverse();
+  }, [feedback]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = 0;
+    }
+  }, [newestFirst.length]);
+
   return (
-    <div className={styles["password-feedback"]}>
-      {feedback.map((fb, index) => (
+    <div className={styles["password-feedback"]} ref={containerRef}>
+      {newestFirst.map((fb, index) => (
         <FeedbackRow key={`fb-${fb.candidate}-${index}`} feedback={fb} />
       ))}
     </div>
