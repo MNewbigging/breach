df005339427659065cf5e84e3e06342eddb61675
Matt Newbigging <mrnewbigging@gmail.com>
2026-03-20 18:28:11 +0000

    pwa test

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/breach/", // replace with the repo name
-  assetsInclude: ["**/*.gltf"],
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-  },
-  publicDir: "public",
-  build: {
-    outDir: "dist",
-  },
+  base: "/breach/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "Breach",
+        short_name: "Breach",
+        description: "Word puzzler",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#000000",
+        icons: [
+          { src: "icon_192.png", sizes: "192x192", type: "image/png" },
+          { src: "icon_512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+      includeAssets: ["dictionary.txt"],
+    }),
+  ],
 });
